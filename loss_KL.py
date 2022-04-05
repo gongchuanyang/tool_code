bc315289e66d3472fe441f79f8a7734975ff1d6b
gongchuanyang <1362444584@qq.com>
2022-04-05 14:04:24 +0800

    Update loss_KL.py

--- loss_KL.py
+++ loss_KL.py
@@ -25,3 +25,43 @@ def loss_KL(student_att, teacher_att):
     single_layer_att_loss = att_ce_loss(teacher_att_selected, student_att)
 
     return single_layer_att_loss
+
+
+
+# method 2
+
+def loss_KL(s_att, t_att):
+    #student_att  [bs,12,seq_len,seq_len]
+    #teacher_att  [bs,64,seq_len,seq_len]
+    
+    bs, s_head, t_head, dim= s_att.size()[0], s_att.size()[1],t_att.size()[1],s_att.size()[2]
+
+    s_att_= s_att.unsqueeze(2).repeat(1,1,t_head,1,1)  # [bs,12,64,seq_len,seq_len]
+    
+    t_att_= t_att.unsqueeze(1).repeat(1,s_head,1,1,1)  # [bs,12,64,seq_len,seq_len]
+    
+
+    s=s_att_.reshape(bs,s_head,t_head,-1)
+    
+    t=t_att_.reshape(bs,s_head,t_head,-1)
+
+    elementwise_prod = torch.mul(s, t)  # [bs,12,64,dim*dim]
+
+    a = F.softmax(elementwise_prod, dim=2)   #(bs, s_head, t_head, dim*dim)
+
+    # s_t_att = torch.max(F.softmax(elementwise_prod, dim=2),dim=2)[0]  #(bs, s_head, dim*dim)
+
+    s_t_att = torch.max(a,dim=2)[0]  #(bs, s_head, dim*dim)
+
+    s_t_att_ = s_t_att.reshape(bs,s_head,dim,dim)
+    
+    # s_t_att_ = torch.max(a,dim=2)[0].reshape(bs,s_head,dim,dim)
+
+    single_layer_att_loss = att_ce_loss(s_t_att_ , s_att)
+
+    return single_layer_att_loss
+
+
+
+
+
